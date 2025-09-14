6f9821902d67bfd0a5f488f9946420e4492b3b23
AndyL200 <aleclair.1105@outlook.com>
2025-09-13 19:15:55 -0500

    pivoting to passbook cus I dont wanna pay $99

--- SamplePage/Program.cs
+++ SamplePage/Program.cs
@@ -5,7 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddSingleton<Wallet_Service>(sp =>
+{
+    //making a blank wallet config, which we do not want
+    var config = sp.GetRequiredService<IConfiguration>();
+    var appleWalletConfig = config.GetSection("AppleWalletConfiguration").Get<apple_wallet.AppleWalletConfiguration>();
+    return new Wallet_Service(appleWalletConfig);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
